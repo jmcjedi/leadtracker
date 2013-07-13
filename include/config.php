6bbba441bc4e13f3b99807bc44dd36cd880a2263
Ubuntu <ubuntu@ip-172-31-2-132.us-west-2.compute.internal>
2013-07-13 18:33:58 +0000

    Added initial phone number for testing.

--- include/config.php
+++ include/config.php
@@ -1,16 +1,16 @@
 <?php
-	require "twilio.php";
-	require "db.php";
-	define('AGENT_NUMBER', '8583829141'); //set this to the phone number you'd like calls forwarded to
-	$ApiVersion = "2010-04-01";
-	$AccountSid = "ACxxxxx";
-	$AuthToken = "xxxxxxxx";
+  require "twilio.php";
+  require "db.php";
+  define('AGENT_NUMBER', '5053494845'); //set this to the phone number you'd like calls forwarded to
+  $ApiVersion = "2010-04-01";
+  $AccountSid = "ACxxxxx";
+  $AuthToken = "xxxxxxxx";
 
-	function format_phone($phone)
-	{
-		$phone = preg_replace("/[^0-9]/", "", $phone);
+  function format_phone($phone)
+  {
+    $phone = preg_replace("/[^0-9]/", "", $phone);
 
-		if(strlen($phone) == 7)
+    if(strlen($phone) == 7)
 			return preg_replace("/([0-9]{3})([0-9]{4})/", "$1-$2", $phone);
 		elseif(strlen($phone) == 10)
 			return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "($1) $2-$3", $phone);
